92c399d2ebda38cda5f1462b5d1d147e7c9dff3d
leopadre <leonardopadre@hotmail.com>
2026-01-22 19:39:19 -0300

    fix: PC1 v2.0

--- Data Layer/silver/ddl.sql
+++ Data Layer/silver/ddl.sql
@@ -1,40 +1,29 @@
---
--- Name: one_big_table; Type: TABLE; Schema: public; Owner: postgres
---
+-- Table: silver.one_big_table
 
-CREATE TABLE public.one_big_table (
-    id integer NOT NULL,
+-- DROP TABLE IF EXISTS silver.one_big_table;
+
+CREATE TABLE IF NOT EXISTS silver.one_big_table
+(
+    id integer NOT NULL DEFAULT nextval('silver.one_big_table_id_seq'::regclass),
     brokered_by real,
-    status text,
+    status text COLLATE pg_catalog."default",
     price real,
     bed integer,
     bath integer,
     acre_lot real,
-    city text,
-    state text,
-    zip_code text,
+    street real,
+    city text COLLATE pg_catalog."default",
+    state text COLLATE pg_catalog."default",
+    zip_code text COLLATE pg_catalog."default",
     house_size real,
     prev_sold_date date,
     price_per_sqft double precision,
     price_per_acre double precision,
-    rooms integer
-);
+    rooms integer,
+    CONSTRAINT one_big_table_pkey PRIMARY KEY (id)
+)
 
-ALTER TABLE public.one_big_table OWNER TO postgres;
+TABLESPACE pg_default;
 
-CREATE SEQUENCE public.one_big_table_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-ALTER TABLE public.one_big_table_id_seq OWNER TO postgres;
-
-ALTER SEQUENCE public.one_big_table_id_seq OWNED BY public.one_big_table.id;
-
-ALTER TABLE ONLY public.one_big_table ALTER COLUMN id SET DEFAULT nextval('public.one_big_table_id_seq'::regclass);
-
-ALTER TABLE ONLY public.one_big_table
-    ADD CONSTRAINT one_big_table_pkey PRIMARY KEY (id);
+ALTER TABLE IF EXISTS silver.one_big_table
+    OWNER to postgres;
